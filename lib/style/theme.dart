6ca8b856b38bed0db2d15ac529199d142dee8465
Sirintip Klaiklueng <koomkun_s@silpakorn.edu>
2022-04-07 16:50:26 +0700

    ฟ

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -4,7 +4,7 @@ class Colors {
 
   const Colors();
 
-  static const Color mainColor = const Color(0xFF151C26);
+  static const Color mainColor = const Color(0xFF93B5F3);
   static const Color secondColor = const Color(0xFFf4C10F);
   static const Color titleColor = const Color(0xFF5a606b);
   
